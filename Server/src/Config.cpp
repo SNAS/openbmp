7db4274a48207ca57b115d69926dc2d5860bc050
Matvey-Kuk <motakuk@gmail.com>
2016-11-11 16:11:58 -0800

    First evpn data published

--- Server/src/Config.cpp
+++ Server/src/Config.cpp
@@ -68,6 +68,7 @@ Config::Config() {
     topic_names_map[MSGBUS_TOPIC_VAR_LS_LINK]          = MSGBUS_TOPIC_LS_LINK;
     topic_names_map[MSGBUS_TOPIC_VAR_LS_PREFIX]        = MSGBUS_TOPIC_LS_PREFIX;
     topic_names_map[MSGBUS_TOPIC_VAR_L3VPN]            = MSGBUS_TOPIC_L3VPN;
+    topic_names_map[MSGBUS_TOPIC_VAR_EVPN]             = MSGBUS_TOPIC_EVPN;
 }
 
 /*********************************************************************//**
