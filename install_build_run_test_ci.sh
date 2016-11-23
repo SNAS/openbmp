feee8c6f0b7817c8e9a9626bf5820a41b247748b
Matvey-Kuk <motakuk@gmail.com>
2016-11-22 16:59:56 -0800

    Test travis

---
+++ install_build_run_test_ci.sh
@@ -0,0 +1,21 @@
+yum install -y gcc gcc-c++ libstdc++-devel boost-devel make cmake git wget openssl-libs openssl-devel cyrus-sasl-devel cyrus-sasl-lib
+
+git clone https://github.com/edenhill/librdkafka.git
+cd librdkafka
+./configure
+make
+make install
+cd ..
+
+git clone https://github.com/jbeder/yaml-cpp.git
+cd yaml-cpp
+mkdir build
+cd build
+cmake -DBUILD_SHARED_LIBS=OFF ..
+make
+make install
+cd ..
+
+mkdir -p build;
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ./
+make ./
