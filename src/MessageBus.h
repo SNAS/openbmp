d6572720dc54c70f60fba134fa6bcbe86f23f7db
Lumin Shi <shilumin@gmail.com>
2021-04-14 21:36:38 -0500

    Added license header info to the source files.

--- src/MessageBus.h
+++ src/MessageBus.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2013-2016 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2019 Lumin Shi.  All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
 #ifndef OPENBMP_MESSAGEBUS_H
 #define OPENBMP_MESSAGEBUS_H
 
--- src/SockBuffer.h
+++ src/SockBuffer.h
@@ -1,6 +1,11 @@
-//
-// Created by Lumin Shi on 2019-07-29.
-//
+/*
+ * Copyright (c) 2013-2016 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2019 Lumin Shi.  All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
 
 #ifndef OPENBMP_SOCKBUFFER_H
 #define OPENBMP_SOCKBUFFER_H
